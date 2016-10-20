845f634d98094216c98333ca1ef1b35f0044e744
terranz <terranz@terranz.mine.nu>
2016-10-21 01:20:57 +0300

    simplify jcommander init

--- src/main/java/ru/terra/mail/Main.java
+++ src/main/java/ru/terra/mail/Main.java
@@ -1,10 +1,9 @@
 package ru.terra.mail;
 
-import org.apache.log4j.BasicConfigurator;
-
 import com.beust.jcommander.JCommander;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.apache.log4j.BasicConfigurator;
 import ru.terra.mail.config.StartUpParameters;
 import ru.terra.mail.gui.StageHelper;
 
@@ -12,15 +11,14 @@ import ru.terra.mail.gui.StageHelper;
  * Created by terranz on 18.10.16.
  */
 public class Main extends Application {
-	public static void main(String... args) {
-		BasicConfigurator.configure();
-		launch(args);
-	}
+    public static void main(String... args) {
+        BasicConfigurator.configure();
+        new JCommander(StartUpParameters.getInstance(), args);
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		final StartUpParameters parameters = StartUpParameters.getInstance();
-		new JCommander(parameters, getParameters().getRaw().toArray(new String[getParameters().getRaw().size()]));
-		StageHelper.openWindow("w_main.fxml", "Main", true);
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StageHelper.openWindow("w_main.fxml", "Main", true);
+    }
 }
