815232ae3386aa9c265d6b602725e273dee3759c
Vadim_Korostelev <terranz@terranz.mine.nu>
2016-10-28 14:43:12 +0300

    fixed updating unread counters

--- src/main/java/ru/terra/mail/gui/model/FoldersModel.java
+++ src/main/java/ru/terra/mail/gui/model/FoldersModel.java
@@ -17,109 +17,110 @@ import java.util.Map;
  */
 public class FoldersModel extends AbstractModel<MailFolder> {
 
-    private TreeItem<FoldersTreeItem> treeRoot;
+	private TreeItem<FoldersTreeItem> treeRoot;
 
-    public TreeItem<FoldersTreeItem> getStoredFolders() {
-        List<MailFolder> folders = null;
-        try {
-            folders = storage.getRootFolders();
-        } catch (Exception e) {
-            logger.error("unable to get folders", e);
-        }
-        if (folders != null && folders.size() > 0) {
-            treeRoot = new TreeItem<>(new FoldersTreeItem(folders.get(0)));
-            folders.get(0).getChildFolders().forEach(cf -> processFolder(treeRoot, cf));
-        } else {
-            MailFolder mf = new MailFolder();
-            mf.setName("Loading...");
-            treeRoot = new TreeItem<>(new FoldersTreeItem(mf));
-        }
-        return treeRoot;
-    }
+	public TreeItem<FoldersTreeItem> getStoredFolders() {
+		List<MailFolder> folders = null;
+		try {
+			folders = storage.getRootFolders();
+		} catch (Exception e) {
+			logger.error("unable to get folders", e);
+		}
+		if (folders != null && folders.size() > 0) {
+			treeRoot = new TreeItem<>(new FoldersTreeItem(folders.get(0)));
+			folders.get(0).getChildFolders().forEach(cf -> processFolder(treeRoot, cf));
+		} else {
+			MailFolder mf = new MailFolder();
+			mf.setName("Loading...");
+			treeRoot = new TreeItem<>(new FoldersTreeItem(mf));
+		}
+		return treeRoot;
+	}
 
-    public TreeItem<FoldersTreeItem> getTreeRoot() {
-        List<MailFolder> folders = getFolders();
-        treeRoot = new TreeItem<>(new FoldersTreeItem(folders.get(0)));
-        folders.get(0).getChildFolders().forEach(cf -> processFolder(treeRoot, cf));
-        return treeRoot;
-    }
+	public TreeItem<FoldersTreeItem> getTreeRoot() {
+		List<MailFolder> folders = getFolders();
+		treeRoot = new TreeItem<>(new FoldersTreeItem(folders.get(0)));
+		folders.get(0).getChildFolders().forEach(cf -> processFolder(treeRoot, cf));
+		return treeRoot;
+	}
 
-    public ObservableList<MailFolder> getFolders() {
-        ObservableList<MailFolder> storedFolders = null;
-        try {
-            storedFolders = storage.getRootFolders();
-        } catch (Exception e) {
-            logger.error("unable to get folders", e);
-        }
-        ObservableList<MailFolder> serverFolders = null;
-        boolean loggedIn = false;
-        try {
-            performLogin();
-            loggedIn = true;
-        } catch (Exception e) {
-            logger.error("Unable to login", e);
-        }
-        if (loggedIn) {
-            try {
-                serverFolders = protocol.listFolders();
-            } catch (MessagingException e) {
-                e.printStackTrace();
-            }
-        }
-        storedFolders = merge(storedFolders, serverFolders);
-        return storedFolders;
-    }
+	public ObservableList<MailFolder> getFolders() {
+		ObservableList<MailFolder> storedFolders = null;
+		try {
+			storedFolders = storage.getRootFolders();
+		} catch (Exception e) {
+			logger.error("unable to get folders", e);
+		}
+		ObservableList<MailFolder> serverFolders = null;
+		boolean loggedIn = false;
+		try {
+			performLogin();
+			loggedIn = true;
+		} catch (Exception e) {
+			logger.error("Unable to login", e);
+		}
+		if (loggedIn) {
+			try {
+				serverFolders = protocol.listFolders();
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+		}
+		storedFolders = merge(storedFolders, serverFolders);
+		return storedFolders;
+	}
 
-    private TreeItem<FoldersTreeItem> processFolder(TreeItem<FoldersTreeItem> parent, MailFolder mailFolder) {
-        TreeItem<FoldersTreeItem> ret = new TreeItem<>(new FoldersTreeItem(mailFolder));
-        parent.getChildren().add(ret);
-        mailFolder.getChildFolders().forEach(cf -> processFolder(ret, cf));
-        return ret;
-    }
+	private TreeItem<FoldersTreeItem> processFolder(TreeItem<FoldersTreeItem> parent, MailFolder mailFolder) {
+		TreeItem<FoldersTreeItem> ret = new TreeItem<>(new FoldersTreeItem(mailFolder));
+		parent.getChildren().add(ret);
+		mailFolder.getChildFolders().forEach(cf -> processFolder(ret, cf));
+		return ret;
+	}
 
-    protected ObservableList<MailFolder> merge(ObservableList<MailFolder> storedFolders,
-                                               ObservableList<MailFolder> serverFolders) {
-        if (storedFolders == null || storedFolders.size() == 0) {
-            storedFolders = FXCollections.observableArrayList();
-            storedFolders.addAll(serverFolders);
-        } else {
-            Map<String, MailFolder> serverFoldersMap = new HashMap<>();
-            List<MailFolder> expandedServerfolders = new ArrayList<>();
-            List<MailFolder> expandedStoredfolders = new ArrayList<>();
-            serverFolders.forEach(sf -> expandedServerfolders.addAll(expandFoldersTree(sf)));
-            storedFolders.forEach(sf -> expandedStoredfolders.addAll(expandFoldersTree(sf)));
-            expandedServerfolders.forEach(f -> serverFoldersMap.put(f.getFullName(), f));
-            for (MailFolder storedFolder : expandedStoredfolders) {
-                MailFolder serverFolder = serverFoldersMap.get(storedFolder.getFullName());
-                if (serverFolder != null)
-                    storedFolder.setFolder(serverFolder.getFolder());
-                else
-                    storedFolder.setDeleted(true);
-            }
-            mergeFoldersTree(storedFolders, serverFoldersMap);
-        }
+	protected ObservableList<MailFolder> merge(ObservableList<MailFolder> storedFolders,
+			ObservableList<MailFolder> serverFolders) {
+		if (storedFolders == null || storedFolders.size() == 0) {
+			storedFolders = FXCollections.observableArrayList();
+			storedFolders.addAll(serverFolders);
+		} else {
+			Map<String, MailFolder> serverFoldersMap = new HashMap<>();
+			List<MailFolder> expandedServerfolders = new ArrayList<>();
+			List<MailFolder> expandedStoredfolders = new ArrayList<>();
+			serverFolders.forEach(sf -> expandedServerfolders.addAll(expandFoldersTree(sf)));
+			storedFolders.forEach(sf -> expandedStoredfolders.addAll(expandFoldersTree(sf)));
+			expandedServerfolders.forEach(f -> serverFoldersMap.put(f.getFullName(), f));
+			for (MailFolder storedFolder : expandedStoredfolders) {
+				MailFolder serverFolder = serverFoldersMap.get(storedFolder.getFullName());
+				if (serverFolder != null) {
+					storedFolder.setFolder(serverFolder.getFolder());
+					storedFolder.setUnreadMessages(serverFolder.getUnreadMessages());
+				} else
+					storedFolder.setDeleted(true);
+			}
+			mergeFoldersTree(storedFolders, serverFoldersMap);
+		}
 
-        storage.storeFolders(storedFolders);
-        return storedFolders;
-    }
+		storage.storeFolders(storedFolders);
+		return storedFolders;
+	}
 
-    private void mergeFoldersTree(List<MailFolder> storedFolders, Map<String, MailFolder> serverFoldersMap) {
-        List<MailFolder> foldersToAdd = new ArrayList<>();
-        for (MailFolder storedFolder : storedFolders)
-            if (!serverFoldersMap.containsKey(storedFolder.getFullName()))
-                foldersToAdd.add(serverFoldersMap.get(storedFolder.getFullName()));
-            else
-                mergeFoldersTree(storedFolder.getChildFolders(), serverFoldersMap);
+	private void mergeFoldersTree(List<MailFolder> storedFolders, Map<String, MailFolder> serverFoldersMap) {
+		List<MailFolder> foldersToAdd = new ArrayList<>();
+		for (MailFolder storedFolder : storedFolders)
+			if (!serverFoldersMap.containsKey(storedFolder.getFullName()))
+				foldersToAdd.add(serverFoldersMap.get(storedFolder.getFullName()));
+			else
+				mergeFoldersTree(storedFolder.getChildFolders(), serverFoldersMap);
 
-        storedFolders.addAll(foldersToAdd);
-    }
+		storedFolders.addAll(foldersToAdd);
+	}
 
-    private List<MailFolder> expandFoldersTree(MailFolder mailFolder) {
-        List<MailFolder> folders = new ArrayList<>();
-        folders.add(mailFolder);
-        if (mailFolder.getChildFolders().size() > 0)
-            mailFolder.getChildFolders().forEach(mf -> folders.addAll(expandFoldersTree(mf)));
-        return folders;
-    }
+	private List<MailFolder> expandFoldersTree(MailFolder mailFolder) {
+		List<MailFolder> folders = new ArrayList<>();
+		folders.add(mailFolder);
+		if (mailFolder.getChildFolders().size() > 0)
+			mailFolder.getChildFolders().forEach(mf -> folders.addAll(expandFoldersTree(mf)));
+		return folders;
+	}
 
 }
